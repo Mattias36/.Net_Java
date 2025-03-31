24c84d6c84f3b51ae476fc279be5652a9003725a
Mattias36 <149504536+Mattias36@users.noreply.github.com>
2025-03-31 21:05:56 +0200

    Add files via upload

---
+++ FizzBuzz/Consoleappdotnet1/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class FizzBuzz
+    {
+        private int _upperLimit;
+
+        public FizzBuzz(int upperLimit)
+        {
+            _upperLimit = upperLimit;
+        }
+
+        public void Run()
+        {
+            for (int i = 1; i <= _upperLimit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Consoleappdotnet1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj górny zakres liczb: ");
+            if (int.TryParse(Console.ReadLine(), out int upperLimit) && upperLimit > 0)
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz(upperLimit);
+                fizzBuzz.Run();
+            }
+            else
+            {
+                Console.WriteLine("Podano nieprawidłową liczbę.");
+            }
+        }
+    }
+}
